cab1870dfda71ee4e02c74dff25dc7d0521de5a6
Sabje9 <146769306+Sabje9@users.noreply.github.com>
2023-10-20 15:59:22 +0200

    yo
    yo

---
+++ PWS Spel/Assets/scripts/InteractionInputData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu fileName = "interactionInputData", menuName = "InteractionSystem/Input")]
+public class NewBehaviourScript : ScriptableObject
+
+    {
+        
+    }
+
